bea496229d1754c30454d07191b4c2adc4a699f1
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-07-11 22:59:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -292,7 +292,7 @@ console.log(`${args[0]} used in ${msg.guild.name} by ${msg.author.username}`)
 }
   if (msg.content.toLowerCase().startsWith(prefix + 'char')) {
   
-  if (msg.content.toLowerCase().startsWith(prefix + 'chars')) return;
+  if (args[0] != '/char') return;
   console.log(`${args[0]} used in ${msg.guild.name} by ${msg.author.username}`)
   
   var charname = args[1]
